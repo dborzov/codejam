226b28fc860b3efb9f1433dc51dea9c786ae6af5
Peter Borzov <peter.borzov@gmail.com>
2015-04-18 01:06:07 -0400

    Problem B, fuck you very much

--- Round 1A/B.py
+++ Round 1A/B.py
@@ -9,10 +9,6 @@ import sys
 DEBUG = True
 
 def log(*args, **kwargs):
-    """
-        for printing all the execution progress messages into stderr
-        (and the output results go to stdout)
-    """
     if not DEBUG:
         return
     kwargs["file"] = sys.stderr
@@ -22,64 +18,42 @@ def log(*args, **kwargs):
 
 
 def solver(N, M):
-    N = N-1
-    def HowManyDoneInXMinutes(X):
-        return sum([X/speed for speed in M])
+    def OutOfLine(X):
+        return sum((X-1)/speed +1 for speed in M)
 
-
-    def WhichOnes(X):
-        return map(lambda speed: X % speed==0, M)
-
-    def LowestAvalBarber(X, which):
-        for i, speed in enumerate(M):
-            if X % speed == 0:
-                if which >0:
-                    which -= 1
-                else:
-                    return i+1
+    def WhichBarber(X, num):
+        for i, barb in enumerate(M):
+            if X % barb != 0:
+                continue
+            if num > 1:
+                num -= 1
+            else:
+                return i+1
         return "fuck"
 
-    last = 25*(N + 100)
-    first = 0
-    est = 3654564564564564575684535
-    while(not (est == N or first + 1 == last)):
-        X = first + (last - first) /2
-        est = HowManyDoneInXMinutes(X)
-        if est > N:
-            last = X
-        if est < N:
-            first = X
+    def AvailableBarbers(X):
+        return [i+1 if X % m ==0 else "x" for i, m in enumerate(M)]
 
+    low = 0
+    high = (N + 1) * 100000
+    while (abs(high - low) > 1):
+        X = low + (high - low)/2
+        val = OutOfLine(X)
+        if val >= N:
+            high = X
+        if val < N:
+            low = X
 
-    while(HowManyDoneInXMinutes(X) >= N):
-        X -= 1
-    X = X+1
+    log("~~~~~~~~~~~~~~~~~")
+    log("range is: (%s,%s) with bounds for %s in [%s, %s]" % (low, high, N, OutOfLine(low), OutOfLine(high)))
+    threshold_val = OutOfLine(low)
+    res = N - threshold_val
+    log("now low is even: D(%s)=%s, R(low)=%s" % (low, threshold_val, res))
+    log("M=",M)
+    log("M=",AvailableBarbers(low))
+    log("~~~~~~~~~~~~~~~~~")
+    return WhichBarber(low, res)
 
-    if HowManyDoneInXMinutes(X) > N:
-        res = N - HowManyDoneInXMinutes(X-1)
-    else:
-        res = 0
-# 91036146
-    if est != N:
-        log("solver is called with: ", N, " und ", M)
-        log("iteratin within [", first, "..", last, "] and done(", X,")=",est, " while N=",N, "bounds (", HowManyDoneInXMinutes(first),",", HowManyDoneInXMinutes(last),")")
-        log('~~~~~~~~~~~~~~~')
-        log("X=", X, "done(", HowManyDoneInXMinutes(X), ")")
-        log("which ones: ", WhichOnes(X))
-        log("while for X=", X-1, "done(", HowManyDoneInXMinutes(X-1), ")")
-        log("which ones: ", WhichOnes(X-1))
-        log("res is ", res)
-        log('~~~~~~~~~~~~~~~')
-        return LowestAvalBarber(X-1, res-1)
-
-
-    return LowestAvalBarber(X, res)
-
-
-
-
-
-    return "Hi "
 
 num_tests = input()
 for i in range(1,num_tests+1):
